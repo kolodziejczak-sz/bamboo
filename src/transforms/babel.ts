2c340ccced7c8999a08376e81a8a5785a35506b2
szymon <s.kolodziejczak95@gmail.com>
2020-09-14 19:31:01 +0200

    babel sourcefile

--- src/transforms/babel.ts
+++ src/transforms/babel.ts
@@ -1,16 +1,14 @@
 import { transform, Loader } from '../esbuild';
-import { getConfig } from '../config';
-import { pathExtension, pathRelative } from '../utils';
+import { pathExtension } from '../utils';
 
 export const transformBabel = async (
     sourceFilePath: string,
     textContent: string
 ) => {
-    const { entryDir } = getConfig();
     const loader = pathExtension(sourceFilePath).slice(1) as Loader;
     const { js } = await transform(textContent, {
         loader,
-        sourcefile: pathRelative(entryDir, sourceFilePath),
+        sourcefile: sourceFilePath,
         sourcemap: 'inline',
     });
 
